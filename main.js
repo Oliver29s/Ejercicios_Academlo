2f8e51d713f2d82282e476a0003e2e93a8ec2374
Oliver29s <mawiis455@gmail.com>
2022-12-12 14:17:07 -0500

    erjercicios

--- main.js
+++ main.js
@@ -1,21 +1,126 @@
-console.log("holaperroshp")
+// Ejercicio 6 Replit
 let  estudiantes = [
-    { nombre: "Mengano", apellido: "Solis", check1: 16, check2: 8},
-    { nombre: "Fulano", apellido: "Rodriguez", check1: 20, check2: 20},
-    { nombre: "Zutano", apellido: "Alvarez", check1: 10, check2: 0},
-    { nombre: "Perengano", apellido: "Leiria", check1: 17, check2: 19}
-] 
-function obtenerSoloLosMejores(estudiantes, nota1, nota2) {
+  { name: "Erik", email: "erik@academlo.com", age: 20 },
+  { name: "Georg", email: "georg@academlo.com", age: 30 },
+  { name: "Daniel", email: "daniel@academlo.com", age: 40 },
+]
+
+function getEmails(users) {
+  
+ 
+  let registrCorreo = []
+  for( let i = 0; i < users.length; i++){
+    registrCorreo.push(users[i].email)
+   
     
-    for( i = 0; i <= estudiantes.length; i++){
-        let nombreApellido = []
-      if( estudiantes[i].check1 >= nota1 && estudiantes[i].check2 >= nota2){
-        console.log(estudiantes[i].check1)
-        console.log(estudiantes[i].check2)
+  }
+
+  
+  return registrCorreo
+  
+}
+
+
+
+// ejercicio 7 Replit
+
+function deleteUser(users, email) {
+  for(let i = 0; i < users.length; i++){
+        if(users[i].email == email){
+            delete users[i]
+            return users
+           
+        }
         
-         nombreApellido.push(estudiantes[i].nombre + " " + estudiantes[i].apellido)
-         return nombreApellido
-      }
     }
-  };
-  console.log(obtenerSoloLosMejores(estudiantes, 15, 15))
+}
+// console.log(deleteUser(estudiantes, "georg@academlo.com"));
+
+// ejercicio 8 replit
+let array1 = [
+  { name: 'Georg', email: 'georg@academlo.com' },
+  { name: 'Andrea', email: 'andrea@gmail.com' }
+]
+
+let array2 = [
+  { email: 'georg@academlo.com', attendance: true },
+  { email: 'andrea@gmail.com', attendance: false }
+]
+
+function mergeData(users, attendances) {
+  
+  
+}
+ // ejercicico 9 replit
+let calificacion = [
+  { name: 'Georg', email: 'georg@academlo.com', score: 100 },
+  { name: 'Andrea', email: 'andrea@gmail.com', score: 70 },
+  { name: 'Andrés', email: 'andres@gmail.com', score: 34 }
+]
+
+ function orderStudentsByScore(students) {
+  
+   
+  
+
+}
+
+
+// ejercicio 10 Replit
+let estuPAis = [
+  { name: 'Georg', email: 'georg@academlo.com', country_id: 1 },
+  { name: 'Andrea', email: 'andrea@gmail.com', country_id: 2 },
+  { name: 'Daniela', email: 'daniela@gmail.com', country_id: 2 },
+  { name: 'Mónica', email: 'monica@gmail.com', country_id: 2 }
+]
+
+let pais = [
+  { id: 1, name: 'Mexico', },
+  { id: 2, name: 'Colombia' }
+]
+
+function countStudents(students, countries, countryName) {
+  let id = []
+  for(i in students){
+    id.push(students[i].country_id)
+  }
+  return id
+
+}
+
+
+
+// ejercicio 12 Replit
+const users = [
+  {
+      name: 'Erik',
+      gender: 'male',
+      age: 22,
+  },
+  {
+      name: 'Daniela',
+      gender: 'female',
+      age: 22,
+  },
+  {
+      name: 'Gustavo',
+      gender: 'male',
+      age: 49,
+  },
+  {
+      name: 'María',
+      gender: 'female',
+      age: 35,
+  },
+];
+
+function findUser(users, name) {
+  for(let i = 0; i < users.length; i ++){
+    if(users[i].name.toLowerCase() == name.toLowerCase()){
+      return users[i]
+    }
+  }
+
+}
+//  ejercicio 13 replit
+
