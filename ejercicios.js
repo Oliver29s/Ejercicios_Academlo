43eda2a2b94c4a04d6c3268a9dd4dc7335648751
Oliver_rodriguez29 <mawiis455@gmail.com>
2023-02-03 21:19:05 -0500

    siuuu

--- ejercicios.js
+++ ejercicios.js
@@ -2,92 +2,113 @@
 let nombre = "Oliver";
 let apellido = "Rodriguez";
 let edad = 22;
-let ciudad = "Soledad-Colombia"
+let ciudad = "Soledad-Colombia";
 
 // ejercicio 2. Suamando valores de un arreglo.
 
-let arrayNumeros = [1, 2, 3]
-let suma = arrayNumeros[0] + arrayNumeros[1] + arrayNumeros[2] 
-
+let arrayNumeros = [1, 2, 3];
+let suma = arrayNumeros[0] + arrayNumeros[1] + arrayNumeros[2];
 
 // ejercicio 3. creando un objeto.
-let user ={
-    nombre:  "Oliver ",
-    apellido: "Rodriguez ",
-    edad: 22,
-}
-let nombreCompleto = user.nombre + user.apellido 
-
+let user = {
+  nombre: "Oliver ",
+  apellido: "Rodriguez ",
+  edad: 22,
+};
+let nombreCompleto = user.nombre + user.apellido;
 
 // ejercicio4. obteniendo correos de usuarios.
-let users =[
-    {
-    nombre:"Oliver",
+let users = [
+  {
+    nombre: "Oliver",
     email: "mawis455@gmail.com",
-    edad: 22
-},
-{
-    nombre:"Samer",
+    edad: 22,
+  },
+  {
+    nombre: "Samer",
     email: "mawiiis455@gmail.com",
-    edad: 22
-},
-{
-    nombre:"William",
+    edad: 22,
+  },
+  {
+    nombre: "William",
     email: "maws455@gmail.com",
-    edad: 22
-},
-{
-    nombre:"Maria",
+    edad: 22,
+  },
+  {
+    nombre: "Maria",
     email: "mawiiss455@gmail.com",
-    edad: 22
-},
-{
-    nombre:"Samer",
+    edad: 22,
+  },
+  {
+    nombre: "Samer",
     email: "maawiis455@gmail.com",
-    edad: 22
-}
+    edad: 22,
+  },
+];
+
+let correos = [
+  users[0].email,
+  users[1].email,
+  users[2].email,
+  users[3].email,
+  users[4].email,
+];
 
 
-]
-
-let correos =[users[0].email,users[1].email,users[2].email,users[3].email,users[4].email,]
 
 
 
-// for (let i = 1; i <= 15; i++) {
-//     if ( i % 3 === 0 && i % 5 === 0) {console.log('FizzBuzz');}
-//     if (i % 3 === 0) {console.log('Fizz');}
-//     if (i % 5 === 0){console.log('Buzz');}
-// }
+const sumNum = (num) => {
+  let newArray = [];
+  for (let i = 1; i < num; i++) {
+    if (i % 3 === 0 || i % 5 === 0) {
+      newArray.push(i);
+    }
+  }
+  let multiplos = newArray.reduce((a, b) => a + b);
+  return multiplos
+};
+console.log(sumNum(30));
+
+
+
+
+
+
+
+
+
+
+
 
 
 const store = () => {
   // code here
-  let vacioArray =[]
+  let vacioArray = [];
   return {
     addProduct(product) {
       // code here
-       vacioArray.push(product)
+      vacioArray.push(product);
     },
     removeProduct(product) {
       // code here
-     let  vacio = vacioArray.find( item => item !== product )
-     vacioArray = [vacio]
+      let vacio = vacioArray.find((item) => item !== product);
+      vacioArray = [vacio];
     },
     listProducts() {
       // code here
-      return vacioArray
-    }
-  }
-}
+      return vacioArray;
+    },
+  };
+};
 
-let  products = [
+let products = [
   {
     id: 1,
-    image:'src/imagen/featured1.png',
+    image: "src/imagen/featured1.png",
     name: "Hoddies",
     category: "Hoddies",
-    price: 14.00,
+    price: 14.0,
     stock: 10,
   },
   {
@@ -95,27 +116,25 @@ let  products = [
     image: "src/imagen/featured3.png",
     name: "Shirts",
     category: "Shirts",
-    price: 24.00,
+    price: 24.0,
     stock: 15,
   },
   {
     id: 3,
-    image: 'src/imagen/home.png',
+    image: "src/imagen/home.png",
     name: "Sweatshirts",
     category: "Sweatshirts",
     stock: 20,
-    price: 24.00,
+    price: 24.0,
   },
 ];
 
+const getUsersData = (users, data) => {
+  let newUsers = [];
+  for (j in users) {
+    newUsers.push(users[j][data]);
+  }
+  return newUsers;
+};
 
-
-
-
-const getUsersData = (users,data) => {
- let newArray = users.map( user => user[data] )
-  return newArray
-}
-
-console.log(getUsersData(products,'image'))
-
+console.log(getUsersData(products, "name"));
